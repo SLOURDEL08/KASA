39c173707a95c3e1eaf7670655403eac28e47d87
SebLOURDEL <seb.lourdel8@gmail.com>
2023-09-03 20:20:26 +0200

    version final

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
     <footer>
        <img width="1000px" height="200px" className="imageFooter" src="logoblanc.png" alt=""/>
-       <span class="mentions">© 20200 Kasa. All rights reserved</span>
+       <span class="mentions">© 2020 Kasa. All rights reserved</span>
     </footer>
   );
 }
